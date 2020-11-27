ac8f1dab2838158f3ee74619be8b78e49abbe779
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-11-27 21:38:14 +0100

    part4 20-22

--- part4/bloglist-backend/middleware/token.js
+++ part4/bloglist-backend/middleware/token.js
@@ -1,35 +1,25 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const { SECRET } = require("../utils/config")
+const { SECRET } = require("../utils/config");
 
-const extractToken = (req,res,next)=> {
-    if (req.headers.authorization){
-        console.log("-----------------------",req.headers.authorization)
-        const token = req.headers.authorization.slice(7)
-         jwt.verify(token,SECRET, (err,decodeToken) => {
-           
-             if (!err) {
-                 req.token = decodeToken
-             }
+const extractToken = (req, res, next) => {
+  if (req.headers.authorization) {
+    const token = req.headers.authorization.slice(7);
+    jwt.verify(token, SECRET, (err, decodeToken) => {
+      if (!err) {
+        req.token = decodeToken;
+      }
+    });
+  }
+  next();
+};
 
-             
-         })
+const authorization = (req, res, next) => {
+  if (!req.token) {
+    throw new Error("not logged");
+  } else {
+    next();
+  }
+};
 
-
-    }
-    next()
-}
-
-const authorization = (req,res,next) => {
-   
-        if(!req.token ){
-        throw new Error("not logged")
-        }
-        else {
-            next()
-        }
-   
-}
-
-
-module.exports= { extractToken , authorization}
+module.exports = { extractToken, authorization };
